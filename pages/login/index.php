f0cd2e60d9e1aa77c120b6310e6e4d2a82bbf5db
tekiana-league <61030777+tekiana-league@users.noreply.github.com>
2020-02-15 21:58:57 -0500

    Add files via upload

--- pages/login/index.php
+++ pages/login/index.php
@@ -30,7 +30,7 @@
 		if (db_verify_conn($link))
 		{
 			// Prepare the statement
-			$sql = "SELECT trainerID, passwordHash, fname, lname, role FROM trainers WHERE trainerID = $1";
+			$sql = "SELECT studentID, passwordHash, fname, lname, role FROM trainers WHERE trainerID = $1";
 			
 			// Attempt to execute the statement
 			$result = db_exec($link, $sql, array(trim($_POST['trainerID'])));
